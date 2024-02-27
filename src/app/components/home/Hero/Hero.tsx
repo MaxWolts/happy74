446736e758620207ad7bbd38683f32fd99fe17e1
MaxWolts <max.wolts@gmail.com>
2024-02-27 11:38:34 -0300

    :sparkles: Se completa Hero

--- src/app/components/home/Hero/Hero.tsx
+++ src/app/components/home/Hero/Hero.tsx
@@ -1,8 +1,13 @@
+import Image from "next/image";
 import { Carousel } from "./Carousel";
 import styles from "./styles.module.css";
 import img1 from "app/../public/hero/slide1.webp";
 import img2 from "app/../public/hero/slide2.webp";
 import img3 from "app/../public/hero/slide3.webp";
+import arrow from "app/../public/hero/svg/arrow.svg";
+import longHeart from "app/../public/hero/svg/heart-with-line.svg";
+import face from "app/../public/hero/svg/face.svg";
+import heartSvg from "app/../public/hero/svg/heart.svg";
 
 const imgs = [
   img1.src,
@@ -22,13 +27,19 @@ export function Hero() {
             <h2>style for you</h2>
           </div>
           <div className={styles.link}>
-            <a href="#" className={styles.link}>START</a>
+            <a href="#" className={styles.link}>
+              <img src={arrow.src} alt="arrow" className={styles.arrowSvg} />
+              START
+            </a>
           </div>
           <div className={styles.containerLines}>
             <div className={styles.lines}></div>
           </div>
         </div>
         <div className={styles.carouselContainer}>
+          <img src={face.src} alt="heart with lines" className={styles.faceSvg} />
+          <img src={longHeart.src} alt="happy face" className={styles.longHeartSvg} />
+          <img src={heartSvg.src} alt="heart" className={styles.heartSvg} />
           <Carousel imgs={imgs} />
         </div>
     </section>
