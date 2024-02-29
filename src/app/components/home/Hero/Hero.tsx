baa79a96a356eea66118c9f3832f9cefa6931de0
MaxWolts <max.wolts@gmail.com>
2024-02-29 08:58:48 -0300

    :art: Se cambia la obtencion de archivos estaticos

--- src/app/components/home/Hero/Hero.tsx
+++ src/app/components/home/Hero/Hero.tsx
@@ -1,18 +1,11 @@
 import Image from "next/image";
 import { Carousel } from "./Carousel";
 import styles from "./styles.module.css";
-import img1 from "app/../public/hero/slide1.webp";
-import img2 from "app/../public/hero/slide2.webp";
-import img3 from "app/../public/hero/slide3.webp";
-import arrow from "app/../public/hero/svg/arrow.svg";
-import longHeart from "app/../public/hero/svg/heart-with-line.svg";
-import face from "app/../public/hero/svg/face.svg";
-import heartSvg from "app/../public/hero/svg/heart.svg";
 
 const imgs = [
-  img1.src,
-  img2.src,
-  img3.src,
+  "/hero/slide1.webp",
+  "/hero/slide2.webp",
+  "/hero/slide3.webp",
 ]
 
 export function Hero() {
@@ -28,7 +21,7 @@ export function Hero() {
           </div>
           <div className={styles.link}>
             <a href="#" className={styles.link}>
-              <img src={arrow.src} alt="arrow" className={styles.arrowSvg} />
+              <img src="/hero/svg/arrow.svg" alt="arrow" className={styles.arrowSvg} />
               START
             </a>
           </div>
@@ -37,9 +30,9 @@ export function Hero() {
           </div>
         </div>
         <div className={styles.carouselContainer}>
-          <img src={face.src} alt="heart with lines" className={styles.faceSvg} />
-          <img src={longHeart.src} alt="happy face" className={styles.longHeartSvg} />
-          <img src={heartSvg.src} alt="heart" className={styles.heartSvg} />
+          <img src="/hero/svg/face.svg" alt="heart with lines" className={styles.faceSvg} />
+          <img src="/hero/svg/heart-with-line.svg" alt="happy face" className={styles.longHeartSvg} />
+          <img src="/hero/svg/heart.svg" alt="heart" className={styles.heartSvg} />
           <Carousel imgs={imgs} />
         </div>
     </section>
--- src/app/components/shop/Shop.tsx
+++ src/app/components/shop/Shop.tsx
@@ -1,10 +1,9 @@
 import { Item } from "./Item";
-import exampleImage from "app/../public/clothes/photo-1520591799316-6b30425429aa.webp";
 
 export const Shop = () => {
   return (
     <div>
-      <Item name="example" img={exampleImage.src} />
+      <Item name="example" img="/clothes/photo-1520591799316-6b30425429aa.webp" />
     </div>
   )
 };
