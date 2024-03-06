cc12113f0d84cc440af0bbbeafdcfbf2db538cd6
MaxWolts <max.wolts@gmail.com>
2024-03-06 11:56:47 -0300

    :sparkles: Se añade conexion con la API de shopify

--- src/app/components/shop/Shop.tsx
+++ src/app/components/shop/Shop.tsx
@@ -1,9 +1,33 @@
 import { Item } from "./Item";
 
-export const Shop = () => {
+interface Product {
+  id: string;
+  title: string;
+  image: {
+    src: string;
+  }
+};
+
+const getProducts = async () => {
+  try {
+    const response = await fetch(`${process.env.SHOPIFY_HOSTNAME}/admin/api/2023-10/products.json`, {
+      headers: new Headers({
+        'X-Shopify-Access-Token': process.env.SHOPIFY_API_KEY || ""
+      })
+    })
+    const data = await response.json();
+    return data
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export const Shop = async () => {
+  const { products } = await getProducts();
+
   return (
     <div>
-      <Item name="example" img="/clothes/photo-1520591799316-6b30425429aa.webp" />
+      {products.map((product: Product) => <Item key={product.id} name={product.title} img={product.image.src} />)}
     </div>
   )
 };
